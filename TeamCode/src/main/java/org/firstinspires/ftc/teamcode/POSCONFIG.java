09f47a7929633978d5bd818dcca31f76b4a570b8
Luke Zhao <pasqalup25@gmail.com>
2026-02-08 13:06:08 -0500

    POSCONFIG

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/POSCONFIG.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+public class POSCONFIG {
+    public static final double FRONT = 0.3872;
+    public static final double BACK = 0.0539;
+    public static final double MIDDLE = 0.2728;
+    //positions for the tapes on the field for intake
+    //TODO: Anshul find these values and implement into your autonomous
+    //public static final double TAPE1RED =
+    //public static final double TAPE2RED =
+    //public static final double TAPE3RED =
+    //public static final double TAPE1BLUE =
+    //public static final double TAPE2BLUE =
+    //public static final double TAPE3BLUE =
+}
